b8311b7290516dee496352ebbea7e30063b71bbb
Igor Malovitsa <igor.malovitsa@blacklane.com>
2020-05-14 14:42:53 +0300

    remove /bin/bash from cmd

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ cat "${OPENVPN_CONFIG}" >> fs/config.ovpn
 
 docker build . -t "socksvpn"
 
-docker run --cap-add=NET_ADMIN --rm -it -p 127.0.0.1:1080:1080 -p 127.0.0.1:5000:5000 socksvpn /bin/bash
+docker run --cap-add=NET_ADMIN --rm -it -p 127.0.0.1:1080:1080 -p 127.0.0.1:5000:5000 socksvpn
